39207d712ecc019cfbee6d345a030c6dc1ca4307
Thipok Ch <thipok.ch@gmail.com>
2023-03-16 15:24:59 +0700

    refactor(model): Separate app and domain model

--- common/model/lib/unself_model.dart
+++ common/model/lib/unself_model.dart
@@ -1,19 +1,19 @@
 library unself_model;
 
-export 'src/account/account.dart';
-export 'src/activity/activity.dart';
-export 'src/app/app.dart';
-export 'src/archive/archive.dart';
-export 'src/archive_data/archive_data.dart';
-export 'src/archive_schema/archive_schema.dart';
-export 'src/archive_schema_part/archive_schema_part.dart';
-export 'src/json_ref/json_ref.dart';
-export 'src/json_ref_list/json_ref_list.dart';
-export 'src/json_ref_value/json_ref_value.dart';
-export 'src/json_schema/json_schema.dart';
+export 'src/domain/account/account.dart';
+export 'src/domain/activity/activity.dart';
+export 'src/domain/app/app.dart';
+export 'src/app/archive/archive.dart';
+export 'src/app/archive_data/archive_data.dart';
+export 'src/app/archive_schema/archive_schema.dart';
+export 'src/app/archive_schema_part/archive_schema_part.dart';
+export 'src/app/json_ref/json_ref.dart';
+export 'src/app/json_ref_list/json_ref_list.dart';
+export 'src/app/json_ref_value/json_ref_value.dart';
+export 'src/app/json_schema/json_schema.dart';
 export 'src/base/base.dart';
-export 'src/settings/settings.dart';
-export 'src/topic/topic.dart';
+export 'src/app/settings/settings.dart';
+export 'src/domain/topic/topic.dart';
 
 export 'src/converter/converter.dart';
 
