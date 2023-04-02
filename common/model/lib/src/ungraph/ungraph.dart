17424f7428ebd1374b380cbd092a9682ad05801f
Thipok Ch <thipok.ch@gmail.com>
2023-04-02 09:42:12 +0700

    refactor: removed unused archive model

---
+++ common/model/lib/src/ungraph/ungraph.dart
@@ -0,0 +1,5 @@
+export 'account/account.dart';
+export 'activity/activity.dart';
+export 'advertiser/advertiser.dart';
+export 'app/app.dart';
+export 'topic/topic.dart';
--- common/model/lib/unself_model.dart
+++ common/model/lib/unself_model.dart
@@ -1,20 +1,10 @@
 library unself_model;
 
-export 'src/app/archive/archive.dart';
-export 'src/app/archive_data/archive_data.dart';
-export 'src/app/archive_schema/archive_schema.dart';
-export 'src/app/archive_schema_part/archive_schema_part.dart';
-export 'src/app/settings/settings.dart';
-
-export 'src/domain/account/account.dart';
-export 'src/domain/activity/activity.dart';
-export 'src/domain/advertiser/advertiser.dart';
-export 'src/domain/app/app.dart';
-export 'src/domain/topic/topic.dart';
-
+export 'src/app/app.dart';
+export 'src/ungraph/ungraph.dart';
 export 'src/unpack/unpack.dart';
 
 export 'src/base/base.dart';
 export 'src/converter/converter.dart';
 
-export 'package:pub_semver/pub_semver.dart';
+// export 'package:pub_semver/pub_semver.dart';
